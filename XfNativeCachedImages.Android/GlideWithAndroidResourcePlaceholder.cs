3447829b6e59fb92ca17d114dbe56534f102dadc
Marco Siccardi <68176309+MSicc@users.noreply.github.com>
2021-03-24 19:40:15 +0100

    implemented Android

---
+++ XfNativeCachedImages.Android/GlideWithAndroidResourcePlaceholder.cs
@@ -0,0 +1,33 @@
+﻿using Android.Glide;
+using Android.Widget;
+using Bumptech.Glide;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace XfNativeCachedImages.Droid
+{
+    public class GlideWithAndroidResourcePlaceholder : IGlideHandler
+    {
+        public GlideWithAndroidResourcePlaceholder()
+        {
+        }
+
+        public bool Build(ImageView imageView, ImageSource source, RequestBuilder builder, CancellationToken token)
+        {
+            if (builder != null)
+            {
+                //easiest && intended way - add the image to Android resources ....
+                //general placeholder:
+                //builder.Placeholder(Resource.Drawable.MSicc_Logo_Base_Blue_1024px_pad25).Into(imageView);
+
+                //error placeholder:
+                builder.Error(Resource.Drawable.MSicc_Logo_Base_Blue_1024px_pad25).Into(imageView);
+
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+}
--- XfNativeCachedImages.Android/MainActivity.cs
+++ XfNativeCachedImages.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Xamarin.Forms;
 
 namespace XfNativeCachedImages.Droid
 {
@@ -16,13 +17,37 @@ namespace XfNativeCachedImages.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            AttachGlide();
+
             LoadApplication(new App());
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+        private void AttachGlide()
+        {
+            Android.Glide.Forms.Init(this, null, false);
+
+            //recommended way of loading resource images=>
+            //Android.Glide.Forms.Init(this, new GlideWithAndroidResourcePlaceholder(), false);
+
+            //Xamarin Forms resource image
+            //Android.Glide.Forms.PlaceholderFromResource("CachedImageTest.MSicc_Logo_Base_Blue_1024px_pad25.png", Assembly.GetAssembly(typeof(MainViewModel)));
+
+            //FontImageSource
+            Android.Glide.Forms.PlaceholderFromFontImageSource(new FontImageSource
+            {
+                Glyph = XfNativeCachedImages.Resources.MaterialDesignIcons.ImageBroken,
+                FontFamily = "MaterialDesignIcons",
+                Color = Xamarin.Forms.Color.Red
+            });
         }
     }
 }
